67632a96279b90d806042c2c68eb83534abbfd2f
alex_ <alex_@172.16.216.192>
2023-01-16 09:10:42 -0500

    Trabajo unidad 03_04

---
+++ src/main/java/vista/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author Andres Ochoa
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        var ventana = new VentanaPedido();
+        var ventana1 = new VentanaProducto();
+        var ventana2 = new VentanaProveedor();
+        
+        ventana.setVisible(true);
+        ventana1.setVisible(true);
+        ventana2.setVisible(true);
+    }
+}
